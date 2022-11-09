e16cc8070cdee1c978c7fc42c06554c2d45702e3
Kássia <kassia_guterres@outlook.com>
2022-11-09 10:08:19 -0300

    moduleResolution included at tsconfig. dotenv configuration.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
-import express from "express";
+import { Request, Response } from "express";
+import express from "express"
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = express();
+server.use(express.json());
 
-server.listen(4000, () => {
-    console.log("Listening on port 4000");
+server.get("/status", (req:Request, res:Response) => {
+    return res.send("Rodando");
+})
+
+
+server.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
 })
